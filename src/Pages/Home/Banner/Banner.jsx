511d5efc9d060635d4b8c942af121355ac5178d1
bikash-dev <bikash.dev.bk@gmail.com>
2023-11-24 21:32:41 +0600

    banner added

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,10 +1,18 @@
+import "./Banner.css";
 
 const Banner = () => {
-    return (
-        <div>
-            <h2>This is Banner</h2>
-        </div>
-    );
+  return (
+    <div className="relative banner_bg">
+      <div className="content flex flex-col items-center absolute top-[60%] left-[10%] md:left-[22%] lg:left-[20%]">
+        <h2 className="text-white font-bold text-2xl lg:text-5xl">
+          Trusted Matrimony & Matchmaking Service
+        </h2>
+        <button className="w-36 py-3 rounded-lg  text-white bg-[#D70F64] hover:bg-transparent border-2 border-[#D70F64] hover:text-[#D70F64] mt-5">
+          Explore
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
