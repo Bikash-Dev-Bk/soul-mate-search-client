c7eea8bdb94f5718bf827fc8c26600de547abb60
bikash-dev <bikash.dev.bk@gmail.com>
2023-11-25 02:08:28 +0600

    SuccessCounter added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../Banner/Banner";
+import SuccessCounter from "../SuccessCounter/SuccessCounter";
 
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <SuccessCounter></SuccessCounter>
         </div>
     );
 };
