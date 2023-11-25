1183cf511dcff3faedfe53fcacbcaaf3283d1f5a
bikash-dev <bikash.dev.bk@gmail.com>
2023-11-25 15:40:07 +0600

    Success Story update

--- src/Pages/Home/SuccessStory/SuccessStory.jsx
+++ src/Pages/Home/SuccessStory/SuccessStory.jsx
@@ -1,32 +1,59 @@
 import { useEffect, useState } from "react";
-import SuccessStoryCard from "./SuccessStoryCard/SuccessStoryCard";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import { Rating } from "@smastrom/react-rating";
+// import SuccessStoryCard from "./SuccessStoryCard/SuccessStoryCard";
+import { BiSolidQuoteLeft } from "react-icons/bi";
+
+import "@smastrom/react-rating/style.css";
+import "swiper/css";
+import "swiper/css/navigation";
 
 const SuccessStory = () => {
+  const [successStory, setSuccessStory] = useState([]);
 
-    const [successStory, setSuccessStory] = useState([]);
+  useEffect(() => {
+    fetch("successStory.json")
+      .then((res) => res.json())
+      .then((data) => {
+        const sortedData = data.sort(
+          (a, b) => new Date(a.marriageDate) - new Date(b.marriageDate)
+        );
+        setSuccessStory(sortedData);
+      });
+  }, []);
 
-    useEffect(() => {
-        fetch("successStory.json")
-          .then((res) => res.json())
-          .then((data) => {
-            const sortedData = data.sort((a, b) => new Date(a.marriageDate) - new Date(b.marriageDate));
-            setSuccessStory(sortedData)
-        });
-      }, []);
+  return (
+    <div className="max-w-[1280px] mx-auto p-5 my-32">
+      <h2 className="text-center text-3xl font-bold mb-16">
+        Matrimony Service with Millions of Success Stories
+      </h2>
 
-    return (
-        <div className="max-w-[1280px] mx-auto p-5 my-32">
-          <h2 className="text-center text-3xl font-bold mb-16">Matrimony Service with Millions of Success Stories</h2>
-          <div className="grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-between items-center">
-            {successStory.slice(0, 6).map((story) => (
-              <SuccessStoryCard
-                key={story.id}
-                story={story}
-              ></SuccessStoryCard>
-            ))}
-          </div>
-        </div>
-      );
-    };
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        {successStory.map((story) => (
+          <SwiperSlide key={story.id} story={story}>
+            <div className="flex flex-col items-center text-justify md:text-center p-8 md:p-12 lg:px-32">
+              <img
+                src={story.coupleImage}
+                alt=""
+                className="w-4/12 rounded-xl"
+              />
+              <Rating
+              className="mt-5"
+                style={{ maxWidth: 180 }}
+                value={story.reviewStar}
+                readOnly
+              />
+              <div className="p-5 text-center ">
+                <h2 className="font-semibold">Marriage Date: {story.marriageDate}</h2>
+                <p className="mt-2 text-justify">{story.successStoryText}</p>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
 
 export default SuccessStory;
