80f323bef2c8322f1f083e8069a3037b03429b90
bikash-dev <bikash.dev.bk@gmail.com>
2023-11-25 23:59:56 +0600

    banner update

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,18 +1,23 @@
+
 import "./Banner.css";
 
 const Banner = () => {
   return (
     <div className="relative banner_bg">
-      <div className="content flex flex-col items-center absolute top-[60%] left-[10%] md:left-[22%] lg:left-[20%]">
-        <h2 className="text-white font-bold text-2xl lg:text-5xl">
-          Trusted Matrimony & Matchmaking Service
-        </h2>
-        <button className="w-36 py-3 rounded-lg  text-white bg-[#D70F64] hover:bg-transparent border-2 border-[#D70F64] hover:text-[#D70F64] mt-5">
-          Explore
-        </button>
+      <div className="overlay"></div>
+      <div className="max-w-5xl h-full mx-auto text-center flex justify-center items-center">
+        <div className="mt-56">
+          <h2 className="text-white font-bold text-2xl lg:text-4xl">
+            Trusted Matrimony & Matchmaking Service
+          </h2>
+          <button className="w-36 py-3 rounded-lg  text-white bg-[#D70F64] hover:bg-transparent border-2 border-[#D70F64] hover:text-[#D70F64] mt-5">
+            Explore
+          </button>
+        </div>
       </div>
     </div>
   );
 };
 
 export default Banner;
+
