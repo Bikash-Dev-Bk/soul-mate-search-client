094f7539c797d642da357b66ec51c8eb766633bf
bikash-dev <bikash.dev.bk@gmail.com>
2023-11-26 01:01:01 +0600

    basic Biodatas added

---
+++ src/Pages/Biodatas/Biodatas/BioDataCard.jsx
@@ -0,0 +1,31 @@
+import PropTypes from "prop-types";
+
+const BioDataCard = ({ data }) => {
+  const { biodataType, profileImage, permanentDivision, age, occupation } =
+    data;
+  return (
+    <div className="shadow-2xl rounded-xl">
+      <img
+        src={profileImage}
+        alt=""
+        className="w-full rounded-tl-xl rounded-tr-xl"
+      />
+      <div className="p-5 font-semibold">
+        <h2>Gender: {biodataType}</h2>
+        <p>Age: {age}</p>
+        <p>Division: {permanentDivision}</p>
+        <p>Occupation: {occupation}</p>
+        <button className="w-full py-3 rounded-lg  text-white bg-[#D70F64] hover:bg-transparent border-2 border-[#D70F64] hover:text-[#D70F64] mt-4">
+          View Profile
+        </button>
+      </div>
+    </div>
+  );
+};
+
+
+BioDataCard.propTypes = {
+  data: PropTypes.object.isRequired,
+};
+
+export default BioDataCard;
--- src/Pages/Biodatas/Biodatas/Biodatas.jsx
+++ src/Pages/Biodatas/Biodatas/Biodatas.jsx
@@ -1,14 +1,32 @@
-import AllBiodatas from "../AllBiodatas/AllBiodatas";
+import { useQuery } from "@tanstack/react-query";
+import useAxiosPublic from "../../../hooks/useAxiosPublic";
 import "./Biodatas.css";
+import BioDataCard from "./BioDataCard";
 
 const Biodatas = () => {
+  const axiosPublic = useAxiosPublic();
+
+  const { data: biodatas = [], refetch } = useQuery({
+    queryKey: ["biodatas"],
+    queryFn: async () => {
+      const res = await axiosPublic.get("/biodatas");
+      return res.data;
+    },
+  });
+
+  console.log("axios", biodatas);
+
   return (
-    <div className="max-w-[1280px] mx-auto p-5 biodatas-container">
+    <div className="max-w-[1280px] mx-auto p-5 biodatas-container my-24">
       <div>
         <h2>Search and filter</h2>
       </div>
 
-      <AllBiodatas></AllBiodatas>
+      <div className="grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-between items-center">
+        {biodatas.map((data) => (
+          <BioDataCard key={data._id} data={data}></BioDataCard>
+        ))}
+      </div>
     </div>
   );
 };
