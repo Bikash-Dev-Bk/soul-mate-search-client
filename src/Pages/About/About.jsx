ebb15c2f02f157995c9998d4751c2198bd6b41bb
bikash-dev <bikash.dev.bk@gmail.com>
2023-11-27 02:13:24 +0600

    How Soul Mate Search Works update and about design

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,5 +1,7 @@
 import { Helmet } from "react-helmet";
 import HeroPages from "../../components/HeroPages/HeroPages";
+import image from "../../assets/Banner/image5.jpg";
+import image2 from "../../assets/Banner/image8.jpg";
 
 const About = () => {
   return (
@@ -8,8 +10,81 @@ const About = () => {
         <title>Soul Mate Search | About</title>
       </Helmet>
       <HeroPages name="About Us"></HeroPages>
-      <div className="max-w-[1280px] mx-auto p-5 my-24">
-        <h2>This is About</h2>
+
+      <div className="max-w-[1280px] mx-auto my-24 p-5 ">
+        <div className="flex gap-10 flex-col md:flex-row">
+          <div className="flex-1">
+            <h3 className="text-2xl font-bold text-[#ff8080]">Who We Are</h3>
+
+            <p className="text-lg text-justify mt-4">
+              Welcome to <span className="font-semibold">Soul Mate Search</span>
+              , where meaningful connections begin and love stories unfold. At{" "}
+              <span className="font-semibold">Soul Mate Search</span>, we
+              believe in the power of love and the journey that brings two
+              hearts together.
+            </p>
+            <p className="text-lg text-justify mt-4">
+              <span className="font-semibold">Soul Mate Search</span> is a team
+              of passionate individuals committed to making the process of
+              finding love as enjoyable and seamless as possible. With a blend
+              of innovative technology and a deep understanding of human
+              connections, we strive to create a space where people can share
+              their stories of love and commitment.
+            </p>
+          </div>
+          <div className="flex md:justify-end flex-1">
+            <img
+              src={image}
+              alt=""
+              className="rounded-tr-[60px] rounded-bl-[60px] md:rounded-tr-[100px] md:rounded-bl-[100px] hover:shadow-2xl "
+            />
+          </div>
+        </div>
+
+        <div className="flex gap-10 flex-col md:flex-row-reverse mt-48">
+          <div className="flex-1">
+            <h3 className="text-2xl font-bold text-[#ff8080]">Our Mission</h3>
+
+            <p className="text-lg text-justify mt-4">
+              Our mission is simple yet profound: to create a platform where
+              individuals can discover their life partners in a safe and trusted
+              environment. We understand the importance of finding a compatible
+              life companion, and we are dedicated to facilitating this
+              beautiful journey.
+            </p>
+            <h3 className="text-2xl font-bold text-[#ff8080] mt-12">
+              Our Story
+            </h3>
+
+            <p className="text-lg text-justify mt-4">
+              <span className="font-semibold">Soul Mate Search</span> was born
+              out of a shared vision to connect hearts and create lasting
+              relationships. Our founders, [Founder Names], envisioned a
+              platform that goes beyond traditional matchmaking, offering a
+              modern and dynamic approach to finding love.
+            </p>
+          </div>
+          <div className="flex md:justify-end flex-1">
+            <img
+              src={image2}
+              alt=""
+              className="rounded-tr-[60px] rounded-bl-[60px] md:rounded-tr-[100px] md:rounded-bl-[100px] hover:shadow-2xl "
+            />
+          </div>
+        </div>
+        <div className="mt-20">
+          <h3 className="text-2xl font-bold text-[#ff8080] ">
+            Join Us on This Journey
+          </h3>
+
+          <p className="text-lg text-justify mt-4">
+            Whether you're embarking on the search for love or have already
+            found your soulmate through <span className="font-semibold">Soul Mate Search</span>, we invite
+            you to be part of our vibrant and growing community. Join us in
+            celebrating love, connections, and the beautiful stories that unfold
+            on our platform. Thank you for choosing <span className="font-semibold">Soul Mate Search</span> as your companion in the journey of love. <br /><br /> With love, <br /> The <span className="font-semibold">Soul Mate Search</span> Team
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/Pages/Home/Works/Works.jsx
+++ src/Pages/Home/Works/Works.jsx
@@ -1,28 +1,36 @@
 import image1 from "../../../assets/love.png";
 import image2 from "../../../assets/search.png";
-import image3 from "../../../assets/conversation.png";
+import image3 from "../../../assets/premium.png";
+import image4 from "../../../assets/conversation.png";
 
 const Works = () => {
   const workProcedure = [
     {
       id: 1001,
       image: image1,
-      name: "Create your profile",
-      description: "Register and create your profile",
+      name: "Create your Biodata",
+      description: "Start your journey by creating a detailed biodata that reflects your personality, values, and preferences. Highlight what makes you unique and what you're looking for in a life partner.",
     },
     {
       id: 1002,
       image: image2,
-      name: "Search your partner",
+      name: "Explore Biodatas",
       description:
-        "Search for your preferred partner who may be located at the other side of the world",
+        "Dive into a diverse pool of biodatas, filtered to match your criteria. Browse through profiles, discover shared interests, and connect with individuals who resonate with your vision for the future.",
     },
     {
       id: 1003,
       image: image3,
+      name: "Premium Membership",
+      description:
+        "Unlock exclusive features with our premium membership, granting you access to direct contact information and featured profiles within our community. Our platform provides a secure space for initiating connections.",
+    },
+    {
+      id: 1004,
+      image: image4,
       name: "Start communication",
       description:
-        "Start communicating with potential partners via mailbox, phone and other means",
+        "Once your contact request is accepted, initiate meaningful conversations with your potential life partner. Our messaging system allows you to connect, share thoughts, and explore compatibility in a private and secure environment.",
     },
   ];
   
@@ -32,16 +40,16 @@ const Works = () => {
         How Soul Mate Search Works
       </h2>
       <p className="text-center text-lg">Get started in 3 easy steps</p>
-      <div className="grid gap-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-between items-center text-center">
+      <div className="grid gap-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-between items-center text-center">
         {workProcedure.map((work) => (
           <div
             key={work.id}
             className="flex flex-col justify-center items-center p-2 md:p-5"
           >
-            <img src={work.image} alt="" />
+            <img src={work.image} className="w-[120px]" alt="" />
             <div className="mt-5">
               <h2 className="mb-3 text-lg font-semibold">{work.name}</h2>
-              <p>{work.description}</p>
+              <p className="text-justify">{work.description}</p>
             </div>
           </div>
         ))}
