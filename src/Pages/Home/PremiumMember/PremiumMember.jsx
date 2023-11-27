020cf2cc5b34a12ece9156861f6ebea2d6288d03
bikash-dev <bikash.dev.bk@gmail.com>
2023-11-27 16:38:17 +0600

    margin update

--- src/Pages/Home/PremiumMember/PremiumMember.jsx
+++ src/Pages/Home/PremiumMember/PremiumMember.jsx
@@ -15,7 +15,7 @@ const PremiumMember = () => {
 
   return (
     <div className="max-w-[1280px] mx-auto p-5 my-32">
-      <h2 className="text-center text-3xl font-bold mb-16">Premium Member Showcase</h2>
+      <h2 className="text-center text-3xl font-bold mb-28">Premium Member Showcase</h2>
       <div className="grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-between items-center">
         {premiumMember.slice(0, 6).map((member) => (
           <PremiumMemberCard
