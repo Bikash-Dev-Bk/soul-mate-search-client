e8fb3d8c988614570fcbe0ea18d41e30e1cc30ae
bikash-dev <bikash.dev.bk@gmail.com>
2023-11-28 19:31:31 +0600

    favourite delete funtionality added

--- src/Pages/Dashboard/FavouritesBiodata/FavouritesBiodata.jsx
+++ src/Pages/Dashboard/FavouritesBiodata/FavouritesBiodata.jsx
@@ -3,23 +3,52 @@ import useAuth from "../../../hooks/useAuth";
 import useAxiosPublic from "../../../hooks/useAxiosPublic";
 import DashBoardHeroPages from "../../../components/DashBoardHeroPages/DashBoardHeroPages";
 import { FaTrashAlt } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const FavouritesBiodata = () => {
   const { user } = useAuth();
   const axiosPublic = useAxiosPublic();
 
-  const { data: myFavBiodata = [] } = useQuery({
-    queryKey: ["menu"],
+  const { data: myFavBiodata = [], refetch } = useQuery({
+    queryKey: ["myFavBiodata"],
     queryFn: async () => {
       const res = await axiosPublic.get(`favourites/${user?.email}`);
       return res.data;
     },
   });
 
+
+  const handleDeleteFavorite = (favorite) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosPublic.delete(`/favourites/${favorite._id}`)
+        .then((res) => {
+          if (res.data.deletedCount > 0) {
+            refetch();
+            Swal.fire({
+              title: "Deleted!",
+              text: `${favorite.name} has been deleted from favourite list.`,
+              icon: "success",
+            });
+          }
+        });
+      }
+    });
+  };
+
+
   return (
     <div>
       <DashBoardHeroPages name="Favourites Bio Data"></DashBoardHeroPages>
-      <div className="px-10">
+      <div className="max-w-[1280px] mx-auto my-12 p-5">
         <table className="w-full leading-normal font-semibold my-12 p-12">
           <thead className="font-bold">
             <tr>
@@ -42,7 +71,7 @@ const FavouritesBiodata = () => {
           </thead>
           <tbody>
             {myFavBiodata.map((favorite) => (
-              <tr key={favorite.biodataId}>
+              <tr key={favorite._id}>
                 <td className="px-5 py-5 border-b border-gray-500 bg-white text-sm">
                   {favorite.name}
                 </td>
@@ -58,7 +87,8 @@ const FavouritesBiodata = () => {
                 <td className="px-5 py-5 border-b border-gray-500 bg-white text-sm">
                   <button 
                   className="text-red-600 hover:text-gray-300 bg-gray-300 hover:bg-red-600 p-4 rounded-full"
-                  // onClick={() => onDeleteButtonClick(favorite.biodataId)}
+                  
+                  onClick={() => handleDeleteFavorite(favorite)}
                   >
                     <FaTrashAlt className=""></FaTrashAlt>
                   </button>
