794c3298a212c73eefc8dabb0e1d30e75aa4c94b
bikash-dev <bikash.dev.bk@gmail.com>
2023-11-29 02:03:31 +0600

    ApprovedContactRequest banner added

--- src/Pages/Dashboard/ApprovedContactRequest/ApprovedContactRequest.jsx
+++ src/Pages/Dashboard/ApprovedContactRequest/ApprovedContactRequest.jsx
@@ -1,11 +1,12 @@
-
+import DashBoardHeroPages from "../../../components/DashBoardHeroPages/DashBoardHeroPages";
 
 const ApprovedContactRequest = () => {
-    return (
-        <div>
-            <h2>ApprovedContactRequest</h2>
-        </div>
-    );
+  return (
+    <div>
+      <DashBoardHeroPages name="Approved Contact Request"></DashBoardHeroPages>
+      <div className="max-w-[1280px] mx-auto my-12 p-5"></div>
+    </div>
+  );
 };
 
 export default ApprovedContactRequest;
