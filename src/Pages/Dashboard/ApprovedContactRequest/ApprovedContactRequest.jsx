663c39d059b66d8b41aa02bef8dea2f818484fb3
bikash-dev <bikash.dev.bk@gmail.com>
2023-11-29 20:34:45 +0600

    ApprovedContactRequest approve button funtionality done

--- src/Pages/Dashboard/ApprovedContactRequest/ApprovedContactRequest.jsx
+++ src/Pages/Dashboard/ApprovedContactRequest/ApprovedContactRequest.jsx
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import DashBoardHeroPages from "../../../components/DashBoardHeroPages/DashBoardHeroPages";
 import useAuth from "../../../hooks/useAuth";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
 
 const ApprovedContactRequest = () => {
   const { user } = useAuth();
@@ -15,7 +16,20 @@ const ApprovedContactRequest = () => {
     },
   });
 
-  console.log("payments", payments);
+  const handleApprovePayment = (payment) => {
+    axiosSecure.patch(`/payments/${payment._id}`).then((res) => {
+      if (res.data.modifiedCount > 0) {
+        refetch();
+        Swal.fire({
+          position: "center",
+          icon: "success",
+          title: `${payment.myBiodataName}'s Contact Request is Approved!`,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    });
+  };
 
   return (
     <div>
@@ -51,14 +65,18 @@ const ApprovedContactRequest = () => {
                 <td className="px-5 py-5 border-b border-gray-500 bg-white text-sm">
                   {payment.myBiodataId}
                 </td>
-                
+
                 <td className="px-5 py-5 border-b border-gray-500 bg-white text-sm">
-                  <button
-                    className="text-white hover:text-[#04AA6D] bg-[#04AA6D] hover:bg-white border-2 border-[#04AA6D] px-4 py-2 rounded-full"
-                    // onClick={() => handleApprovePayment(payment)}
-                  >
-                    Approve
-                  </button>
+                  {payment.status === "Approved" ? (
+                    <p className="text-[#04AA6D]">Approved</p>
+                  ) : (
+                    <button
+                      className="text-white hover:text-[#04AA6D] bg-[#04AA6D] hover:bg-white border-2 border-[#04AA6D] px-4 py-2 rounded-full"
+                      onClick={() => handleApprovePayment(payment)}
+                    >
+                      Approve
+                    </button>
+                  )}
                 </td>
               </tr>
             ))}
