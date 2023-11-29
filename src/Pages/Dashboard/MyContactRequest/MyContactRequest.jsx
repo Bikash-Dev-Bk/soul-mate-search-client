0dc23c121cc350cf4d1fa4bf98d3bd7ddfa8adb3
bikash-dev <bikash.dev.bk@gmail.com>
2023-11-29 19:58:09 +0600

    MyContactRequest delete method added

--- src/Pages/Dashboard/MyContactRequest/MyContactRequest.jsx
+++ src/Pages/Dashboard/MyContactRequest/MyContactRequest.jsx
@@ -3,14 +3,13 @@ import DashBoardHeroPages from "../../../components/DashBoardHeroPages/DashBoard
 import useAuth from "../../../hooks/useAuth";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import { FaTrashAlt } from "react-icons/fa";
-
-
+import Swal from "sweetalert2";
 
 const MyContactRequest = () => {
   const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
 
-  const { data: payments = [] } = useQuery({
+  const { data: payments = [], refetch } = useQuery({
     queryKey: ["payments", user.email],
     queryFn: async () => {
       const res = await axiosSecure.get(`/payments/${user.email}`);
@@ -18,7 +17,30 @@ const MyContactRequest = () => {
     },
   });
 
-  
+  const handleDeletePayment = (payment) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/payments/${payment._id}`).then((res) => {
+          if (res.data.deletedCount > 0) {
+            refetch();
+            Swal.fire({
+              title: "Deleted!",
+              text: `${payment.name} has been deleted from Payment list.`,
+              icon: "success",
+            });
+          }
+        });
+      }
+    });
+  };
 
   return (
     <div>
@@ -37,7 +59,7 @@ const MyContactRequest = () => {
                 Status
               </th>
               <th className="px-5 py-3 border-b-2 border-gray-500 bg-gray-200 text-left text-md  text-gray-700 uppercase tracking-wider">
-               Mobile No.
+                Mobile No.
               </th>
               <th className="px-5 py-3 border-b-2 border-gray-500 bg-gray-200 text-left text-md  text-gray-700 uppercase tracking-wider">
                 Email
@@ -68,7 +90,7 @@ const MyContactRequest = () => {
                 <td className="px-5 py-5 border-b border-gray-500 bg-white text-sm">
                   <button
                     className="text-red-600 hover:text-gray-300 bg-gray-300 hover:bg-red-600 p-4 rounded-full"
-                    // onClick={() => handleDeletePayment(payment)}
+                    onClick={() => handleDeletePayment(payment)}
                   >
                     <FaTrashAlt className=""></FaTrashAlt>
                   </button>
