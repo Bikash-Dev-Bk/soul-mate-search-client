f7fa3844f38f109671755f81d9a9047330325b46
bikash-dev <bikash.dev.bk@gmail.com>
2023-11-29 20:51:14 +0600

    AdminDashboard Total Revenue added

--- src/Pages/Dashboard/AdminDashboard/AdminDashboard.jsx
+++ src/Pages/Dashboard/AdminDashboard/AdminDashboard.jsx
@@ -1,8 +1,12 @@
 import DashBoardHeroPages from "../../../components/DashBoardHeroPages/DashBoardHeroPages";
 import { useEffect } from "react";
 import { useState } from "react";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
 
 const AdminDashboard = () => {
+  const axiosSecure = useAxiosSecure();
+
   const [biodatas, setBiodatas] = useState([]);
   const [maleBiodatas, setMaleBiodatas] = useState([]);
   const [femaleBiodatas, setFemaleBiodatas] = useState([]);
@@ -12,9 +16,10 @@ const AdminDashboard = () => {
     fetch("http://localhost:5000/biodatas")
       .then((res) => res.json())
       .then((data) => {
-        
         const male = data.filter((biodata) => biodata.biodataType === "Male");
-        const female = data.filter((biodata) => biodata.biodataType === "Female");
+        const female = data.filter(
+          (biodata) => biodata.biodataType === "Female"
+        );
         const premium = data.filter((biodata) => biodata.isPremium === true);
 
         setMaleBiodatas(male);
@@ -23,6 +28,17 @@ const AdminDashboard = () => {
         setBiodatas(data);
       });
   }, []);
+
+  const { data: payments = [] } = useQuery({
+    queryKey: ["payments"],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/payments`);
+      return res.data;
+    },
+  });
+
+  const totalPrice = payments.reduce((total, item) => total + item.price, 0);
+
 
   return (
     <div>
@@ -47,7 +63,7 @@ const AdminDashboard = () => {
           </div>
           <div className="flex flex-col justify-center items-center text-center bg-[#04AA6D] p-5 rounded-lg text-white">
             <h2 className="text-xl">Total Revenue</h2>
-            <p className="text-3xl font-bold">{}</p>
+            <p> <span className="text-3xl font-bold">{totalPrice}</span> <span className="font-semibold">BDT</span></p>
           </div>
         </div>
       </div>
