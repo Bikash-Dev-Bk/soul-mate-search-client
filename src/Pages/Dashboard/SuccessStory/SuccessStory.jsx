b98a6a09080a84cd1ff69c05960a9ba68b214111
bikash-dev <bikash.dev.bk@gmail.com>
2023-11-30 15:16:04 +0600

    modal added at SuccessStory

--- src/Pages/Dashboard/SuccessStory/SuccessStory.jsx
+++ src/Pages/Dashboard/SuccessStory/SuccessStory.jsx
@@ -1,6 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 import DashBoardHeroPages from "../../../components/DashBoardHeroPages/DashBoardHeroPages";
 import useAxiosPublic from "../../../hooks/useAxiosPublic";
+import Modal from "./Modal";
+import { useState } from "react";
 
 const SuccessStory = () => {
   const axiosPublic = useAxiosPublic();
@@ -13,6 +15,11 @@ const SuccessStory = () => {
     },
   });
 
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => setModalOpen(true);
+  const closeModal = () => setModalOpen(false);
+
   return (
     <div>
       <DashBoardHeroPages name="Success Story"></DashBoardHeroPages>
@@ -24,7 +31,7 @@ const SuccessStory = () => {
                 Male Biodata Id
               </th>
               <th className="px-5 py-3 border-b-2 border-gray-500 bg-gray-200 text-left text-md  text-gray-700 uppercase tracking-wider">
-              Female Biodata Id
+                Female Biodata Id
               </th>
 
               <th className="px-5 py-3 border-b-2 border-gray-500 bg-gray-200 text-left text-md  text-gray-700 uppercase tracking-wider">
@@ -43,9 +50,26 @@ const SuccessStory = () => {
                 </td>
 
                 <td className="px-5 py-5 border-b border-gray-500 bg-white text-sm">
-                  <button className="text-white hover:text-[#04AA6D] bg-[#04AA6D] hover:bg-white border-2 border-[#04AA6D] px-4 py-2 rounded-full">
+                  <button
+                    onClick={openModal}
+                    className="text-white hover:text-[#04AA6D] bg-[#04AA6D] hover:bg-white border-2 border-[#04AA6D] px-4 py-2 rounded-full"
+                  >
                     View Story
                   </button>
+                  <Modal isOpen={isModalOpen} onClose={closeModal}>
+                    <div className="p-6">
+                      <h1 className="text-2xl font-semibold mb-4">
+                        Success Story
+                      </h1>
+                      <p>{story.successStoryText}</p>
+                      <button
+                        onClick={closeModal}
+                        className="mt-4 bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+                      >
+                        Close Modal
+                      </button>
+                    </div>
+                  </Modal>
                 </td>
               </tr>
             ))}
