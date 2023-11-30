3ea314b0b4cdcffddf5651b0b386cf51aaa7fcde
bikash-dev <bikash.dev.bk@gmail.com>
2023-11-30 19:02:03 +0600

    filter design

--- src/Pages/Biodatas/BioDataFilter/BioDataFilter.jsx
+++ src/Pages/Biodatas/BioDataFilter/BioDataFilter.jsx
@@ -1,77 +1,51 @@
-import { useForm } from "react-hook-form";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
+import Select from "react-select";
 
 const BioDataFilter = () => {
-  const { register, handleSubmit, reset } = useForm();
-
-  const onSubmit = async (data) => {};
-
   return (
-    <div>
-      <h2>Filter Bio Data</h2>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        {/* biodata type */}
-        <div className="form-control w-full my-6">
-          <label className="label">
-            <span className="label-text ">Biodata type*</span>
-          </label>
-          <select
-            defaultValue="default"
-            {...register("category", { required: true })}
-            className="select select-bordered w-full text-lg py-2 border-2 border-[#04AA6D] focus:outline-none mt-2"
-          >
-            <option disabled value="default">
-              Select a category
-            </option>
-            <option value="Male">Male</option>
-            <option value="Female">Female</option>
-          </select>
-        </div>
+    <div className="mr-8">
+      <h2 className="text-lg font-semibold mb-4">Filters</h2>
 
-        {/* age */}
-        <div className="form-control w-full my-6">
-          <label className="label">
-            <span className="label-text">Age*</span>
-          </label>
-          <select
-            defaultValue="default"
-            {...register("category", { required: true })}
-            className="select select-bordered w-full text-lg py-2 border-2 border-[#04AA6D] focus:outline-none mt-2"
-          >
-            <option disabled value="default">
-              Select a category
-            </option>
-            <option value="salad">20 to 25</option>
-            <option value="pizza">26 to 30</option>
-            <option value="soup">31 to 35</option>
-            <option value="dessert">36 to 40</option>
-            <option value="drinks">40 to 50</option>
-            <option value="drinks">50+</option>
-          </select>
+      {/* Age Range Slider */}
+      <div className="mb-6">
+        <label className="block text-sm font-medium mb-2">Age Range</label>
+        <Slider range min={0} max={100} defaultValue={[0, 100]} />
+        <div className="flex justify-between text-sm text-gray-600 mt-2">
+          <span></span> - <span></span>
         </div>
+      </div>
 
-        {/* division  */}
-        <div className="form-control w-full my-6">
-          <label className="label">
-            <span className="label-text">Division*</span>
-          </label>
-          <select
-            defaultValue="default"
-            {...register("category", { required: true })}
-            className="select select-bordered w-full text-lg py-2 border-2 border-[#04AA6D] focus:outline-none mt-2"
-          >
-            <option disabled value="default">
-              Select a category
-            </option>
-            <option value="Dhaka">Dhaka</option>
-            <option value="Chattogram">Chattogram</option>
-            <option value="Rangpur">Rangpur</option>
-            <option value="Barisal">Barisal</option>
-            <option value="Khulna">Khulna</option>
-            <option value="Mymensingh">Mymensingh</option>
-            <option value="Sylhet">Sylhet</option>
-          </select>
-        </div>
-      </form>
+      {/* Biodata Type Dropdown */}
+      <div className="mb-6">
+        <label className="block text-sm font-medium mb-2">Biodata Type</label>
+        <Select
+          options={[
+            { value: "all", label: "All" },
+            { value: "Male", label: "Male" },
+            { value: "Female", label: "Female" },
+          ]}
+        />
+      </div>
+
+      {/* Division Dropdown */}
+      <div>
+        <label className="block text-sm font-medium mb-2">
+          Permanent Division
+        </label>
+        <Select
+          options={[
+            { value: "all", label: "All" },
+            { value: "Dhaka", label: "Dhaka" },
+            { value: "Chattogram", label: "Chattogram" },
+            { value: "Rangpur", label: "Rangpur" },
+            { value: "Barisal", label: "Barisal" },
+            { value: "Khulna", label: "Khulna" },
+            { value: "Mymensingh", label: "Mymensingh" },
+            { value: "Sylhet", label: "Sylhet" },
+          ]}
+        />
+      </div>
     </div>
   );
 };
--- src/Pages/Biodatas/Biodatas/Biodatas.jsx
+++ src/Pages/Biodatas/Biodatas/Biodatas.jsx
@@ -2,25 +2,20 @@ import { useQuery } from "@tanstack/react-query";
 import useAxiosPublic from "../../../hooks/useAxiosPublic";
 import HeroPages from "../../../components/HeroPages/HeroPages";
 import { Helmet } from "react-helmet";
-import { useForm } from "react-hook-form";
 import BioDataCard from "../BioDataCard/BioDataCard";
 import BioDataFilter from "../BioDataFilter/BioDataFilter";
 import "./Biodatas.css";
 
-
 const Biodatas = () => {
   const axiosPublic = useAxiosPublic();
-  const { register, handleSubmit, reset } = useForm();
 
-  const { data: biodatas = [], refetch } = useQuery({
+  const { data: biodatas = [] } = useQuery({
     queryKey: ["biodatas"],
     queryFn: async () => {
       const res = await axiosPublic.get("/biodatas");
       return res.data;
     },
   });
-
-  const onSubmit = async (data) => {};
 
   return (
     <div>
