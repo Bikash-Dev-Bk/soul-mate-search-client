634e6d2d7c6b1ceb343f4363772db9c084cac47b
bikash-dev <bikash.dev.bk@gmail.com>
2023-12-02 14:16:41 +0600

    AdminDashboard banner remove and data load by axios and tanstack query

--- src/Pages/Dashboard/AdminDashboard/AdminDashboard.jsx
+++ src/Pages/Dashboard/AdminDashboard/AdminDashboard.jsx
@@ -1,34 +1,23 @@
-import DashBoardHeroPages from "../../../components/DashBoardHeroPages/DashBoardHeroPages";
-import { useEffect } from "react";
-import { useState } from "react";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import { useQuery } from "@tanstack/react-query";
 import Chart from "./Chart";
+import useAxiosPublic from "../../../hooks/useAxiosPublic";
 
 const AdminDashboard = () => {
+  const axiosPublic = useAxiosPublic();
   const axiosSecure = useAxiosSecure();
 
-  const [biodatas, setBiodatas] = useState([]);
-  const [maleBiodatas, setMaleBiodatas] = useState([]);
-  const [femaleBiodatas, setFemaleBiodatas] = useState([]);
-  const [premiumBiodatas, setPremiumBiodatas] = useState([]);
+  const { data: biodatas = [] } = useQuery({
+    queryKey: ["biodatas"],
+    queryFn: async () => {
+      const res = await axiosPublic.get("/biodatas");
+      return res.data;
+    },
+  });
 
-  useEffect(() => {
-    fetch("https://soul-mate-search-server.vercel.app/biodatas")
-      .then((res) => res.json())
-      .then((data) => {
-        const male = data.filter((biodata) => biodata.biodataType === "Male");
-        const female = data.filter(
-          (biodata) => biodata.biodataType === "Female"
-        );
-        const premium = data.filter((biodata) => biodata.isPremium === true);
-
-        setMaleBiodatas(male);
-        setFemaleBiodatas(female);
-        setPremiumBiodatas(premium);
-        setBiodatas(data);
-      });
-  }, []);
+  const male = biodatas.filter((biodata) => biodata.biodataType === "Male");
+  const female = biodatas.filter((biodata) => biodata.biodataType === "Female");
+  const premium = biodatas.filter((biodata) => biodata.isPremium === true);
 
   const { data: payments = [] } = useQuery({
     queryKey: ["payments"],
@@ -42,7 +31,6 @@ const AdminDashboard = () => {
 
   return (
     <div>
-      <DashBoardHeroPages name="Admin Dashboard"></DashBoardHeroPages>
       <div className="max-w-[1280px] mx-auto my-12 p-5">
         <div className="grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-5">
           <div className="flex flex-col justify-center items-center text-center bg-[#04AA6D] p-5 rounded-lg text-white">
@@ -51,15 +39,15 @@ const AdminDashboard = () => {
           </div>
           <div className="flex flex-col justify-center items-center text-center bg-[#04AA6D] p-5 rounded-lg text-white">
             <h2 className="text-xl">Male Biodata</h2>
-            <p className="text-3xl font-bold">{maleBiodatas.length}</p>
+            <p className="text-3xl font-bold">{male.length}</p>
           </div>
           <div className="flex flex-col justify-center items-center text-center bg-[#04AA6D] p-5 rounded-lg text-white">
             <h2 className="text-xl">Female Biodata</h2>
-            <p className="text-3xl font-bold">{femaleBiodatas.length}</p>
+            <p className="text-3xl font-bold">{female.length}</p>
           </div>
           <div className="flex flex-col justify-center items-center text-center bg-[#04AA6D] p-5 rounded-lg text-white">
             <h2 className="text-xl">Premium Biodata</h2>
-            <p className="text-3xl font-bold">{premiumBiodatas.length}</p>
+            <p className="text-3xl font-bold">{premium.length}</p>
           </div>
           <div className="flex flex-col justify-center items-center text-center bg-[#04AA6D] p-5 rounded-lg text-white">
             <h2 className="text-xl">Total Revenue</h2>
@@ -72,9 +60,9 @@ const AdminDashboard = () => {
         </div>
         <Chart
           biodatas={biodatas}
-          maleBiodatas={maleBiodatas}
-          femaleBiodatas={femaleBiodatas}
-          premiumBiodatas={premiumBiodatas}
+          maleBiodatas={male}
+          femaleBiodatas={female}
+          premiumBiodatas={premium}
         ></Chart>
       </div>
     </div>
