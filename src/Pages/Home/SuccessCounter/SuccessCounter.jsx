3469405c88e435f24957791293384c9de273af79
bikash-dev <bikash.dev.bk@gmail.com>
2023-12-08 02:11:32 +0600

    SuccessCounter data fetch update with hooks

--- src/Pages/Home/SuccessCounter/SuccessCounter.jsx
+++ src/Pages/Home/SuccessCounter/SuccessCounter.jsx
@@ -1,37 +1,30 @@
 import { FaUsers, FaFemale, FaMale } from "react-icons/fa";
 import { MdOutlineDownloadDone } from "react-icons/md";
 import "./SuccessCounter.css";
-import { useEffect, useState } from "react";
+import useAxiosPublic from "../../../hooks/useAxiosPublic";
+import { useQuery } from "@tanstack/react-query";
 
 const SuccessCounter = () => {
-  const [marriageComplete, setMarriageComplete] = useState([]);
+  const axiosPublic = useAxiosPublic();
 
-  useEffect(() => {
-    fetch("https://soul-mate-search-server.vercel.app/stories")
-      .then((res) => res.json())
-      .then((data) => {
-        setMarriageComplete(data);
-      });
-  }, []);
+  const { data: marriageComplete = [] } = useQuery({
+    queryKey: ["marriageComplete"],
+    queryFn: async () => {
+      const res = await axiosPublic.get("/stories");
+      return res.data;
+    },
+  });
 
-  const [biodatas, setBiodatas] = useState([]);
-  const [maleBiodatas, setMaleBiodatas] = useState([]);
-  const [femaleBiodatas, setFemaleBiodatas] = useState([]);
+  const { data: biodatas = [] } = useQuery({
+    queryKey: ["biodatas"],
+    queryFn: async () => {
+      const res = await axiosPublic.get("/biodatas");
+      return res.data;
+    },
+  });
 
-  useEffect(() => {
-    fetch("https://soul-mate-search-server.vercel.app/biodatas")
-      .then((res) => res.json())
-      .then((data) => {
-        const male = data.filter((biodata) => biodata.biodataType === "Male");
-        const female = data.filter(
-          (biodata) => biodata.biodataType === "Female"
-        );
-
-        setMaleBiodatas(male);
-        setFemaleBiodatas(female);
-        setBiodatas(data);
-      });
-  }, []);
+  const male = biodatas.filter((biodata) => biodata.biodataType === "Male");
+  const female = biodatas.filter((biodata) => biodata.biodataType === "Female");
 
   return (
     <div className="bg-success bg-fixed max-w-[1280px] mx-auto my-32 p-5 md:12 lg:p-24">
@@ -47,12 +40,12 @@ const SuccessCounter = () => {
         <div className="flex flex-col justify-center items-center text-center bg-[#04AA6D] p-5 rounded-lg text-white">
           <FaFemale className="text-5xl" />
           <h2 className="text-2xl font-semibold my-2">Girls Bio Data</h2>
-          <h3 className="text-5xl font-bold">{femaleBiodatas.length}</h3>
+          <h3 className="text-5xl font-bold">{female.length}</h3>
         </div>
         <div className="flex flex-col justify-center items-center text-center bg-[#04AA6D] p-5 rounded-lg text-white">
           <FaMale className="text-5xl" />
           <h2 className="text-2xl font-semibold my-2">Boys Bio Data</h2>
-          <h3 className="text-5xl font-bold">{maleBiodatas.length}</h3>
+          <h3 className="text-5xl font-bold">{male.length}</h3>
         </div>
         <div className="flex flex-col justify-center items-center text-center bg-[#04AA6D] p-5 rounded-lg text-white">
           <MdOutlineDownloadDone className="text-5xl" />
